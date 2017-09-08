da1ffdb6a6dbd201cd9eb65cc1371f5ae724c82f
Kelsie <Kelsie@10.150.6.210>
2017-09-07 22:05:54 -0500

    Fun fact, this is dumb :)

--- homework01/tomerlin.java
+++ homework01/tomerlin.java
@@ -1,9 +1,23 @@
 package homework01;
 
+import java.io.*;
+import java.net.*;
+
 /**
+ * cite:
+ * https://stackoverflow.com/questions/2165006/simple-java-client-server-program
  *
  * @author Kelsie
  */
 public class tomerlin {
-    
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket test = new ServerSocket(500);
+        int port = test.getLocalPort();
+        Socket socket = new Socket("11.144.9.921", 500);
+        System.out.println("Connection accepted: " + socket);
+        DataInputStream dIS = new DataInputStream(System.in);
+        System.out.println("");
+    }
 }
